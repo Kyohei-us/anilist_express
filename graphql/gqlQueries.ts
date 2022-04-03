06eac028c17bb33b18e69361b7d953ce006fd575
Kyohei-us <kyohei1212000@gmail.com>
2022-04-03 01:48:15 -0700

    Change: GetMedia query

--- graphql/gqlQueries.ts
+++ graphql/gqlQueries.ts
@@ -1,25 +1,23 @@
 import { gql } from 'graphql-tag';
 
 export const GetMedia = gql`
-    query GetMedia($page: Int, $perPage: Int, $type: MediaType, $sort: [MediaListSort]) {
+    query GetMedia($page: Int, $perPage: Int, $type: MediaType, $sort: [MediaSort]) {
         Page(page: $page, perPage: $perPage) {
-            mediaList(type: $type, sort: $sort, userName: "k3n3n12") {
-          		media {
-                    title {
-                        english
-                        native
-                        romaji
-                        userPreferred
-                    }
-                    type
-                    description
-                    id
-                    idMal
-                    favourites
-                    format
-                    genres
-                    hashtag                
+            media(type: $type, sort: $sort) {
+                title {
+                    english
+                    native
+                    romaji
+                    userPreferred
                 }
+                type
+                description
+                id
+                idMal
+                favourites
+                format
+                genres
+                hashtag
             }
         }
     }
